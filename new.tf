0bc3b77fb2e1affe6319a5bbd86e4c51223c44d9
mohitshtech <82747316+mohitshtech@users.noreply.github.com>
2021-10-22 15:13:35 +0530

    new tf file

---
+++ new.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "apps-server" {
+  ami           = "ami-02e136e904f3da870"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "mohit-server-new-tf"
+    env = "test1"
+  }
+}
